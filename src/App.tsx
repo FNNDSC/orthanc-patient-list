c73b3ffdb3b145aac8185f4f4b22fae7eab82ef3
Jennings Zhang <dev@sl.jennin.xyz>
2025-08-01 21:33:30 -0400

    Add status bar and theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,26 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { PatientsTable } from "./table";
 import "@patternfly/react-core/dist/styles/base.css";
 import { useMemo } from "preact/hooks";
+import { StatusBar } from "./StatusBar";
+import { Flex, FlexItem, Page, PageSection } from "@patternfly/react-core";
+import styles from "./styles.module.css";
 
 export function App() {
 	const queryClient = useMemo(() => new QueryClient(), []);
 	return (
 		<QueryClientProvider client={queryClient}>
-			<PatientsTable />
+			<Flex
+				direction={{ default: "column" }}
+				justifyContent={{ default: "justifyContentSpaceBetween" }}
+				className={styles.page}
+			>
+				<FlexItem style={{ minHeight: "100%", backgroundColor: "green" }}>
+					<PatientsTable />
+				</FlexItem>
+				<FlexItem className={styles.statusBar}>
+					<StatusBar />
+				</FlexItem>
+			</Flex>
 		</QueryClientProvider>
 	);
 }
